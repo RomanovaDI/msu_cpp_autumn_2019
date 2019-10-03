098ab104d0c5ffa48454162a47e731ffd33b846c
Daria Romanova <romanovadi@gmail.com>
2019-10-03 22:05:43 +0300

    Zero homework is ready

---
+++ 00/sum_by_rows.cpp
@@ -0,0 +1,45 @@
+#include <chrono>
+#include <iostream>
+#include <algorithm>    // std::generate
+#include <vector>       // std::vector
+#include <ctime>        // std::time
+#include <cstdlib>      // std::rand, std::srand
+
+class Timer
+{
+    using clock_t = std::chrono::high_resolution_clock;
+    using microseconds = std::chrono::microseconds;
+public:
+    Timer()
+        : start_(clock_t::now())
+    {
+    }
+
+    ~Timer()
+    {
+        const auto finish = clock_t::now();
+        const auto us = 
+            std::chrono::duration_cast<microseconds>
+                (finish - start_).count();
+        std::cout << us << " us" << std::endl;
+    }
+
+private:
+    const clock_t::time_point start_;
+};
+
+int RandomNumber () { return (std::rand()%100); }
+
+int main()
+{
+    Timer t;
+    int size = 10000;
+    std::srand ( unsigned ( std::time(0) ) );
+    std::vector<int> myvector (size * size);
+    std::generate (myvector.begin(), myvector.end(), RandomNumber);
+    int sum = 0;
+    for (int i = 0; i < size; ++i)
+        for (int j = 0; j < size; j++)
+            sum += myvector[i * size + j];
+    return 0;
+}
