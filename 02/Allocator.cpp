a2b4aea628d712db18a596dcee51d65945bf9f8a
Daria Romanova <romanovadi@gmail.com>
2019-10-18 17:26:26 +0300

    Runtime error

--- 02/Allocator.cpp
+++ 02/Allocator.cpp
@@ -25,13 +25,13 @@ void* LinearAllocator::alloc(size_t size) {
     size_t tmp_size = size % allign ? size + 1 : size;
     try {
         if (tmp_size > sizeAll - sizeUsed)
-            throw "not enough memory";
+            throw std::runtime_error("not enough memory");
         void *tmp_return = pointerStart + sizeUsed;
         sizeUsed += tmp_size;
         return tmp_return;
     }
-    catch (const char *str) {
-        std::cerr << "error: " << str << '\n';
+    catch (std::runtime_error& e) {
+        std::cerr << "error: " << e.what() << '\n';
         return nullptr;
     }
 }
